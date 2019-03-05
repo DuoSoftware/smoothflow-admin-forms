c41b57031f4b5f432de3002c8118165dff4d92eb
Shehan Tissera <shehanproductions@ymail.com>
2019-03-05 17:23:20 +0530

    removed unwanted files

---
+++ js/control.signin.js
@@ -0,0 +1,7 @@
+/* use strict */
+
+mainModule.controller('SignInCtrl', function ($scope, $auth, $mdDialog, $base64, AppConfigurations) {
+    console.log('signin controller');
+
+   
+});
---
+++ js/control.signup.js
@@ -0,0 +1,7 @@
+/* use strict */
+
+mainModule.controller('SignUpCtrl', function ($scope, $auth, $mdDialog, $state, $stateParams) {
+    console.log('signup controller');
+
+    
+});
